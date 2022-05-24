45282bbb01b452295b9b60611355abfae07d754a
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-05-24 08:11:30 -0300

    I changed a sentence from the footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
 import { FooterContainer, Copyright, SocialMedia } from "./styles";
 
 import Logo from "../../assets/Logo_base.png";
-import logoInstagram from "../../assets/logoInstagram.svg"
-import logoFacebook from "../../assets/logoFacebook.svg"
-import logoYoutube from "../../assets/logoYoutube.svg"
+import logoInstagram from "../../assets/logoInstagram.svg";
+import logoFacebook from "../../assets/logoFacebook.svg";
+import logoYoutube from "../../assets/logoYoutube.svg";
 
 export function Footer() {
   return (
@@ -11,32 +11,43 @@ export function Footer() {
       <Copyright>
         <div className="Logo">
           <img src={Logo} alt="Logo CompAmostra" />
-          <a href="https://www.cesupa.br/" target="_blank" rel="noreferrer">CompAmostra</a>
+          <a href="https://www.cesupa.br/" target="_blank" rel="noreferrer">
+            CompAmostra
+          </a>
         </div>
+        <p>©2022 - Cesupa CompAmostra</p>
+        <p>Todos os direitos reservados.</p>
         <p>
-          ©2022 - Cesupa CompAmostra
-        </p >
-        <p>
-          Todos os direitos reservados.
-        </p>
-        <p>
-          <a 
-          href="https://www.ryanolivr.dev/panelinha/" 
-          target="_blank" 
-          rel="noreferrer">
-            Feito com ♡ pela panelinha
-            </a>
+          <a
+            href="https://www.ryanolivr.dev/panelinha/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Feito com ♡ pela Panelinha
+          </a>
         </p>
       </Copyright>
       <SocialMedia>
-        <a href="https://www.instagram.com/cesupaonline/" target="_blank" rel="noreferrer">
-        <img src={logoInstagram} alt="Logo do Instagram" />
+        <a
+          href="https://www.instagram.com/cesupaonline/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={logoInstagram} alt="Logo do Instagram" />
         </a>
-        <a href="https://www.facebook.com/CesupaOnline" target="_blank" rel="noreferrer">
-        <img src={logoFacebook} alt="Logo do Facebook" />
+        <a
+          href="https://www.facebook.com/CesupaOnline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={logoFacebook} alt="Logo do Facebook" />
         </a>
-        <a href="https://www.youtube.com/c/CesupaOnline" target="_blank" rel="noreferrer">
-        <img src={logoYoutube} alt="Logo do Youtube" />
+        <a
+          href="https://www.youtube.com/c/CesupaOnline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={logoYoutube} alt="Logo do Youtube" />
         </a>
       </SocialMedia>
     </FooterContainer>
